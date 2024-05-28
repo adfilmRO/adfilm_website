232697ac83a90179c93dc833c4ec1e55517d72bd
eduardneague <eduard.neague1337@gmail.com>
2024-05-28 18:54:30 +0300

    removed section

---
+++ components/StaffCard.tsx
@@ -0,0 +1,61 @@
+"use client";
+
+import React from "react";
+import { StaffCardProps } from "@/utils/types";
+import Image from "next/image";
+import Link from "next/link";
+import { motion } from "framer-motion";
+
+import { urlFor } from "@/app/lib/sanity";
+
+const StaffCard = ({
+  name,
+  image_src,
+  instagramLink,
+  position,
+}: StaffCardProps) => {
+  return (
+    <>
+      <motion.div
+        initial={{ opacity: 0, y: -50 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        viewport={{ once: true }}
+        transition={{
+          duration: 1,
+          type: "spring",
+          stiffness: 100,
+          delay: 0.15,
+        }}
+        className="staffCard staffCardBorder gap-3 text-white w-[280px] h-[392px] items-center rounded-2xl flex flex-col px-1 py-2"
+      >
+        <img
+          src={urlFor(image_src).url()}
+          alt={name + "_portrait"}
+          className="w-[260px] select-none shadow-sm shadow-purple-900 h-[263px] rounded-lg staffCardBorder"
+          draggable={false}
+        />
+        <Link
+          href={instagramLink}
+          className="instagram-tag flex gap-2 p-3 rounded-lg"
+        >
+          <Image
+            src="/assets/instagram_logo.svg"
+            alt="instagram logo"
+            width={16}
+            height={16}
+            draggable={false}
+            className="w-auto h-auto select-none"
+          />
+          <h1 className="text-white font-mustica-semibold text-[13px]">
+            {name}
+          </h1>
+        </Link>
+
+        <h1 className="font-montserrat text-[12px]">{position}</h1>
+        <div className="w-[80%] h-[1px] bg-gradient-to-r from-transparent via-gray-500 to-transparent" />
+      </motion.div>
+    </>
+  );
+};
+
+export default StaffCard;
--- components/index_page/RestOfMainPage.tsx
+++ components/index_page/RestOfMainPage.tsx
@@ -32,9 +32,9 @@ const RestOfMainPage = async () => {
       <main className="restOfMainPage-section cool-background-i-dont-have-yet w-full flex flex-col justify-center items-center relative">
         <div className="absolute hidden lg:block top-0 left-0 bg-gradient-to-b from-black opacity-100 to-transparent w-full h-[5rem] " />
         <div className="wrapper lg:w-[95%] xl:w-[90%] 1780px:w-[70%] max-w-[130rem] w-full flex flex-col justify-center items-center">
-          <LuamArta data = {luamArtaData} />
+          <LuamArta data={luamArtaData} />
           <CineSuntem data={staffCardData} />
-          <CeaMaiBunaMotivatie data={copyData} />
+          {/* <CeaMaiBunaMotivatie data={copyData} /> */}
           <BehindTheScenes data={behindTheScenesData} />
           <AlteServiciiOferite />
           <ParteneriSiColaboratori data={parteneriData} />
